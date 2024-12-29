976e82598e6a3ce959fd492300255d9b30e70043
zsh <zsh@ev5.pro>
2024-12-29 12:45:40 +0200

    start.sh loop

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-while ! uvicorn main_new:app; do
-    echo "restarting.."
+while :
+do
+    hypercorn app.app:app -b 0.0.0.0:8099 \
+        --workers 10 \
+        --log-config logging.conf \
+        --backlog 9200 \
+        --graceful-timeout 2 \
+        --access-logfile access.log \
+        --error-logfile error.log \
+        --keep-alive 10
+
+        # --max-requests 1800 \
+        # --max-requests-jitter 400 \
+    sleep 1
 done
