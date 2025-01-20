dead6db614d02c6bc0b0e9269540395b4817c67d
zsh <zsh@ev5.pro>
2025-01-21 00:02:01 +0200

    fix localization

--- app/tools/localization.py
+++ app/tools/localization.py
@@ -1,4 +1,4 @@
-from app.app import db, app
+from app.app import db
 from app.tools.utils import Config
 from cachetools import TTLCache, cached
 
@@ -18,9 +18,10 @@ class Localization():
         languages = "lang_" + languages
         
         localization: dict = await db.queryDictByKey(query=
-            f"SELECT {languages} FROM locale WHERE lang_en != '' AND lang_en != NULL AND uri = 'antibot.py'", 
+            f"SELECT {languages} FROM locale WHERE lang_en != '' AND uri = 'antibot.py'", 
             args={}, 
             ifnull='lang_en')
+        # if int(Config("debug"))==1: print(f"LOCALIZATION: load [{localization}]")
         for k, v in localization.items():
             for lang in self.LANGUAGES:
                 if lang not in self.LOCALE_STRINGS:
@@ -31,24 +32,29 @@ class Localization():
                         lang_value = k
                     self.LOCALE_STRINGS[lang_key.replace("lang_", "")][k] = lang_value
 
+        # if int(Config("debug"))==1: print(f"LOCALIZATION: LOADED {self.LOCALE_STRINGS}")
         self.loaded = True
         
     async def Save(self) -> None:
         for text in self.saveQueue.copy():
+            if int(Config("debug"))==1: print(f"LOCALIZATION: save[{text}]")
             await db.insert(table="locale", data={
                     "lang_en" : text,
                     "uri" : "antibot.py",
-                })
+                }, ignore_into=True)
         self.saveQueue.clear()
 
-    @cached(cache=TTLCache(maxsize=1024, ttl=60))
+    @cached(cache=TTLCache(maxsize=1024*100, ttl=60))
     def Translate(self, language: str, string: str) -> str:
         if not self.loaded:
+            if int(Config("debug"))==1: print("LOCALIZATION: lang not loaded")
             return string
         
         if string not in self.LOCALE_STRINGS[language]:
             self.saveQueue.add(string)
-            return string
+            if int(Config("debug"))==1: print(f"LOCALIZATION: not in strings [{string}]")
+            for lang in self.LANGUAGES:
+                self.LOCALE_STRINGS[lang][string] = string
         
         return self.LOCALE_STRINGS[language][string]
     
